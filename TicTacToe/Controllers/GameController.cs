80d0d7818593ec9710674ba29bd9dc39f69bce3a
Johan Oskrason <scopolamine@mail.ru>
2023-03-15 16:04:16 +0600

    Ready

---
+++ TicTacToe/Controllers/GameController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Common;
+using System.Numerics;
+using System.Reflection.PortableExecutable;
+using TicTacToe.Models;
+
+namespace TicTacToe.Controllers
+{
+    public class GameController : Controller
+    {
+        private static Dictionary<int, Game> games = new Dictionary<int, Game>();
+        private static int nextId = 1;
+
+        [HttpGet]
+        public IActionResult Index() 
+        { 
+            return View(games.Values); 
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string player1, string player2)
+        {
+            Game game = new Game();
+            game.Id = nextId++;
+            game.Player1 = player1;
+            game.Player2 = player2;
+            games.Add(game.Id, game);
+            return RedirectToAction("Play", new { id = game.Id });
+        }
+
+        [HttpGet]
+        public IActionResult Play(int id)
+        {
+            Game game = games[id];
+            return View(game);
+        }
+
+        [HttpGet]
+        public IActionResult Move(int id, string player, int row, int column)
+        {
+            Game game = games[id];
+            game.MakeMove(player, row, column);
+            return View("Play", game);
+        }
+
+        [HttpGet]
+        public ActionResult Reset(int id)
+        {
+            Game game = games[id];
+            game.ResetBoard();
+            games[id] = game;
+            return RedirectToAction("Play", new { id = id });
+        }
+    }
+}
---
+++ TicTacToe/Models/Game.cs
@@ -0,0 +1,107 @@
+﻿namespace TicTacToe.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Player1 { get; set; }
+        public string Player2 { get; set; }
+        public char[,] Board { get; set; }
+        public string Status { get; set; }
+        public string CurrentPlayer { get; set; }
+        public bool IsGameOver { get; set; }
+        public bool Success { get; set; }
+
+        public Game()
+        {
+            Board = new char[3, 3];
+            ResetBoard();
+            CurrentPlayer = Player1;
+        }
+
+        public void ResetBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Board[i, j] = '.';
+                }
+            }
+            IsGameOver = false;
+        }
+
+        public void MakeMove(string player, int row, int column)
+        {
+            if (player != CurrentPlayer || IsGameOver)
+            {
+                Success = false;
+                return;
+            }
+            if (Board[row, column] != '.')
+            {
+                Success = false;
+                return;
+            }
+            Board[row, column] = (player == Player1) ? 'X' : 'O';
+            if (CheckForWin(Board))
+            {
+                IsGameOver = true;
+                Success = true;
+                return;
+            }
+            if (CheckForTie(Board))
+            {
+                IsGameOver = true;
+                Success = false;
+                return;
+            }
+            CurrentPlayer = (CurrentPlayer == Player1) ? Player2 : Player1;
+            Success = true;
+            return;
+        }
+
+        private bool CheckForWin(char[,] board)
+        {
+            // Check rows
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] != '.' && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    return true;
+                }
+            }
+
+            // Check columns
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[0, j] != '.' && board[0, j] == board[1, j] && board[1, j] == board[2, j])
+                {
+                    return true;
+                }
+            }
+
+            // Check diagonals
+            if (board[1, 1] != '.' && ((board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) || (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckForTie(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == '.')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
